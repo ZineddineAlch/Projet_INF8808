0fc5070eeef0897bbee8d16521dfb307f7bc80d8
= <=>
2023-06-29 18:28:12 -1000

    doc

--- server.py
+++ server.py
@@ -1,9 +1,19 @@
+'''
+    Contains the server to run our application.
+'''
 from flask_failsafe import failsafe
 
 
 @failsafe
 def create_app():
-    from app import app
+    '''
+        Gets the underlying Flask server from our Dash app.
+
+        Returns:
+            The server to be run
+    '''
+    # the import is intentionally inside to work with the server failsafe
+    from app import app  # pylint: disable=import-outside-toplevel
     return app.server
 
 
